ab4f9c50f424a8224dcf4dc8c030a41c1d7dfe88
CS-PAPUN <phukamoll12345656@gmail.com>
2026-08-25 22:42:22 +0700

    Update target_max_consec.c

--- week6/target_max_consec.c
+++ week6/target_max_consec.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 int main(){
-    int n,i=0,gg,x,y,before,current,streak=0,best_streak;
+    int x,n;
+    int count = 0;       // นับจำนวน x
+    int streak = 0;
+    int best_streak = 0;
     scanf("%d", &x);
-    scanf("%d", &y);
-    before=y;
-    if(y==x){
-        streak+=1;
-    }
     while(1){
         scanf("%d", &n);
-        if(n!=0){
-            current=n;
-            if(current==x){
-                i++;
-                if(current==before){
-                    streak++;
-                    before=current;
-                }else{
-                    before=current;
-                    if(streak>0){
-                        streak*=0;
-                    }
-                }
+        if(n==0){break;}
+        if (n == x) {
+            count++;
+            streak++;
+            if (streak > best_streak) {
+                best_streak = streak;
             }
-        }else{break;}
-        printf("%d <--- ", gg);
-        printf("%d -<-<- ", before);
-        printf("%d <-<-<\n", current);
+        }else{
+            streak = 0;
+        }
     }
-    best_streak=gg;
-    if(i==0){printf("None");}
-    else{printf("%d\n%d", best_streak,i);}
+    printf("%d\n%d", best_streak,count);
 }
